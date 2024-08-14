64b50ef76f1b78331727d122d784c6e43e9e884d
vaibhav <vaibhavgarg3210@gmail.com>
2024-08-14 23:06:36 +0530

    1.Add Rate LimiterAdd Rate Limiter in ApiGateWay using redis as well as by resilience4j.reatelimiter

--- accounts/src/main/java/com/learn/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/learn/accounts/controller/AccountsController.java
@@ -25,6 +25,7 @@ import com.learn.accounts.dto.ErrorResponseDto;
 import com.learn.accounts.dto.ResponseDto;
 import com.learn.accounts.service.IAccountsService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -133,8 +134,15 @@ public class AccountsController {
 	@Operation(summary = "Get Java version", description = "Get Java versions details that is installed into accounts microservice")
 	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
 			@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
+	@RateLimiter(name = "getJavaVersion",fallbackMethod = "getJavaVersionFallback")
 	@GetMapping("/java-version")
 	public ResponseEntity<String> getJavaVersion() {
+		logger.info("Start Of getJavaVersion ");
+		return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+	}
+	
+	public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+		logger.info("Start Of getJavaVersion 2");
 		return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
 	}
 
